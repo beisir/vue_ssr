ba09da536d38661944d43817a9af8314374cadef
beisir <hsy7426@163.com>
2019-03-23 15:50:38 +0800

    高级组件

--- client/components/tabs/index.js
+++ client/components/tabs/index.js
@@ -1,7 +1,7 @@
 import Tabs from './tabs.vue'
 import Tab from './tab.vue'
 
-export default function(Vue) {
-    Vue.component(Tabs.name, Tabs)
-    Vue.component(Tab.name, Tab)
+export default function (Vue) {
+  Vue.component(Tabs.name, Tabs)
+  Vue.component(Tab.name, Tab)
 };
